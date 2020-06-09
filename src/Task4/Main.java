5a02a2cbd330e6bb3a0b2a3856c3db6c86df7d4b
Natali11111 <58171677+Natali11111@users.noreply.github.com>
2020-06-09 13:21:32 +0300

    Cosmetic changes

--- src/Task4/Main.java
+++ src/Task4/Main.java
@@ -1,12 +1,10 @@
 package Task4;
 
-import java.math.BigDecimal;
-
 public class Main {
     public static void main(String[] args) {
-BankAccount bankAccount = new BankAccount(23, 78.0);
-bankAccount.credit(12.9);
-bankAccount.debit(20.03);
+        BankAccount bankAccount = new BankAccount(23, 78.0);
+        bankAccount.credit(12.9);
+        bankAccount.debit(20.03);
 
     }
 }
